a776018bfb1ba1b426b33581fdc5373e4f45ae88
erihidayat06 <erihidayat549@gmail.com>
2025-02-03 20:45:00 +0700

    first commit

---
+++ projek/projeck.js
@@ -0,0 +1,109 @@
+projek = [
+  // Sibuadi
+  {
+    nama_projek: "Website Sibudi",
+    gambar: "img/sibudi.png",
+    deskripsi:
+      "Website Sibudi merupakan website laporan keuangan desa dari pemeritahan brebes ",
+    bahasa: ["img/html.png", "img/css.png", "img/js.png", "img/php.png"],
+    framework: ["img/laravel.png", "img/bootstrap.png"],
+    link: "https://sibudi.brebeskab.go.id/",
+  },
+  //   Cineplex
+  {
+    nama_projek: "Website Pemesanan Tiket Bioskop (CinePlex) ",
+    gambar: "img/cineplex.png",
+    deskripsi:
+      "Website pemesanan tiket bioskop (CinePlex) adalah website yang digunakan untuk melakukan pemesanan tiket bioskop. Website ini di bangun saat saya mengikuti MSIB Kampus merdengan dengan membangun sebuah REST API. ",
+    bahasa: ["img/html.png", "img/css.png", "img/js.png"],
+    framework: ["img/express-js.png", "img/bootstrap.png"],
+    link: "https://github.com/trainerfe5gamelab/Tiket-Bioskop",
+  },
+
+  //   My Makeup
+  {
+    nama_projek: "Website My Makeup ",
+    gambar: "img/mymakeup.png",
+    deskripsi: `<p>
+            Website MyMakeup ini saya buat untuk tugas web programing II yang
+            sudah menggunakan framework Laravel.
+          </p>
+          <p>
+            Website ini adalah website yang dimana digunakan untuk meningkatkan
+            usaha para Makeup Artis (MUA) agar mudah di jumpai para kostumer.
+            kostumer juga tidak perlu lagi datang langsung ke salon untuk
+            memesan Makeup artis.
+          </p>
+          <p>
+            Selain untuk tugas kuliah website ini juga saya kembangin lagi untuk
+            mengikuti lomba
+            <strong>Krenova 2023 di Kota Tegal</strong> dan alhamdulilah
+            mendapatkan juara harapan III bersama tim saya.
+          </p>`,
+    bahasa: ["img/html.png", "img/css.png", "img/js.png", "img/php.png"],
+    framework: ["img/laravel.png", "img/bootstrap.png"],
+    link: "https://github.com/erihidayat06/mymakeup",
+  },
+
+  //
+  {
+    nama_projek: "Website Kunjungan Perpus ",
+    gambar: "img/kunjungan-perpus.png",
+    deskripsi:
+      "Website Kunjungan Perpustakaan ini saya buat saat saya melakukan PKL di MTs N 1 Tegal yang digunakan untuk mempermudah petugas perpus saat melakukan rekap kunjungan perpustakaan yang semualanya manual menggunakan buku sekarang sudah otomotis karena menggunakan website ",
+    bahasa: ["img/html.png", "img/css.png", "img/js.png", "img/php.png"],
+    framework: ["img/laravel.png", "img/bootstrap.png"],
+    link: "https://github.com/erihidayat06/kunjungan-perpus",
+  },
+
+  {
+    nama_projek: "Aplikasi Keuangan SIBUDI",
+    gambar: "img/keuangan-sibudi.png",
+    deskripsi:
+      "Website aplikasi keuangan SIBUDI merupakan aplikasi untuk mengelola keuangan diBumdes yang bisa di gunakan oleh semua bumdes di indonesia yang berbentuk akuntansi ada neraca, laporan labarugi, perubahan aruskas, perubahan modal dan lain sebagainya.",
+    bahasa: ["img/html.png", "img/css.png", "img/js.png", "img/php.png"],
+    framework: ["img/laravel.png", "img/bootstrap.png"],
+    link: "https://apksibudi.my.id/",
+  },
+  {
+    nama_projek: "Website Situga",
+    gambar: "img/situga.png",
+    deskripsi:
+      "Website situga merupakan website laporan harga barang yang ada di pasar tradisonal kabupaten brebes.  ",
+    bahasa: ["img/html.png", "img/css.png", "img/js.png", "img/php.png"],
+    framework: ["img/laravel.png", "img/bootstrap.png"],
+    link: "https://github.com/erihidayat06/harga-pasar",
+  },
+  {
+    nama_projek: "Website Cafe Journey ",
+    gambar: "img/cofe-journey.png",
+    deskripsi: ` <p>
+            Website cafe journey adalah website pencarian cafe agar membermudah
+            pelanggan cafe mencari cafe yang recomendasi didalam website
+            tersebut memiliki berbagai fitur diantaranya adalah sebagai berikut:
+          </p>
+          <ul>
+            <li>Mencari kafe</li>
+            <li>Ulasan Cafe</li>
+            <li>Profil Cafe</li>
+            <li>Alamat Cafe</li>
+            <li>Jadwal Cafe</li>
+            <li>Memilih Menu di cafe</li>
+            <li>Reservasi cafe</li>
+            <li>Daftar Cafe</li>
+            <li>Admin Cafe</li>
+          </ul>`,
+    bahasa: ["img/html.png", "img/css.png", "img/js.png", "img/php.png"],
+    framework: ["img/laravel.png", "img/bootstrap.png"],
+    link: "https://github.com/erihidayat06/cafe-journey",
+  },
+  {
+    nama_projek: "Five Day",
+    gambar: "img/five-day.png",
+    deskripsi:
+      "Aplikasi Five day ini saya buat ketika MK Dasar Pemprograman saat belajar python website ini sebagai aplikasi kasit toko baju  ",
+    bahasa: ["img/python.png"],
+    framework: [],
+    link: "https://github.com/erihidayat06/five-day-py",
+  },
+];
